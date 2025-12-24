ea1a6b187a04c2535ef00fb9e19670052d672bcd
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2025-12-24 19:57:33 +0530

    new pattern added

--- Basics/pattern.java
+++ Basics/pattern.java
@@ -88,5 +88,32 @@ public class pattern {
             }
             System.out.println();
         }
+        //butterfly pattern
+        for(int i = 1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int space = 2* (n-i);
+            for(int j=1;j<=space ;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int space = 2* (n-i);
+            for(int j=1;j<=space ;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
