87ab993b6234f9cfcf4baa7bb80551edd5217446
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2025-12-25 23:54:24 +0530

    added new fact priogram in fun

--- Basics/javalect7.java
+++ Basics/javalect7.java
@@ -1,13 +1,23 @@
 import java.util.*;
 public class javalect7 {
-    public static void PrintMyName(String Name){
-        System.out.println(Name);
+    public static void PrintFactorial(int n){
+        int Factorial = 1;
+        if(n<0){
+        System.out.println("invalid input ");
         return;
+
+        }
+        for(int i=n;i>=1;i--){
+            Factorial = Factorial*i;
+        }
+        System.out.println(Factorial);
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String Name = sc.next();
-        PrintMyName(Name);
+        int n = sc.nextInt();
+       
+        PrintFactorial(n);
+
 
     }
 }
