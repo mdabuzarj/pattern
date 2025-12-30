226fbeccaae7aac68068538833d83841747430fb
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2025-12-30 20:22:31 +0530

    new lect 12

---
+++ Basics/lect12.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class lect12 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        //nextline for full name
+        String name = sc.next();
+
+        for(int i=0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+        //.length() for len of str
+        //System.out.println(name.length());
+
+    }
+    
+}
