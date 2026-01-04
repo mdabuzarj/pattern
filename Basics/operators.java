08c4faaa259da72771442f48e5472433a8a6b887
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2026-01-04 22:20:49 +0530

    .

---
+++ Basics/operators.java
@@ -0,0 +1,13 @@
+public class operators {
+    public static void main(String args[]){
+        int a = 10;
+        int b =  0;
+        //pre increment
+        //a++ post increment
+
+        b = a++;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
